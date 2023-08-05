e166a016ba18860df6770b3e1bce683e6b4e2518
taetaerin <taerin13@naver.com>
2023-08-06 00:45:40 +0900

    참여하기 버튼 수정

--- screens/ParContent.js
+++ screens/ParContent.js
@@ -10,88 +10,107 @@ const ParContent = ({route, navigation}) => {
     const [parComplete, setParComplete] = useState(false);
 
   return (
-    <SafeAreaView style={{backgroundColor: 'white', flex: 1}} >
-        <View>
-            {/* 상단바 */}
-            <View 
-                style={{
-                width: '100%', 
-                backgroundColor: 'white', 
-                height: 44, 
-                alignItems: 'center',
-                paddingHorizontal: 18,
-                flexDirection: 'row',
-                justifyContent:'space-between',
-                }}
-                >
-                    <TouchableOpacity>
-                      <Ionic name="chevron-back-sharp" style={{fontSize:24}} onPress={() => navigation.goBack()} />
-                  </TouchableOpacity>
-                    <TouchableOpacity>
-                    <Ionic name= "md-ellipsis-horizontal" size={17} color='#424242' />
-                    </TouchableOpacity>
-            </View>
-
+    <View style={{flex: 1}}>
+        <SafeAreaView style={{backgroundColor: 'white'}} >
             <View>
-                <Image source={data.image} style={{width: '100%', height: 240}} />
-            </View>
-
-            <View style={styles.wrapper}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <View style={styles.a}>
-                    <Text style={styles.situation}>{data.situation}</Text>
-                </View>
-                <Ionic style={styles.icon} name='person-outline' size={16} color="#424242"> 5</Ionic>
-                
-                </View>
-                <Text style={styles.title}>
-                {data.title}
-                </Text>
-               <View style={{flexDirection: 'row', marginTop: 20 ,marginVertical: 10}}>
-                <Image source={data.avatar} style={styles.avatar}></Image>
-                <Text style={styles.name}>{data.name}</Text>
-               </View>
-
-               <Text style={styles.subtitle}>모임정보</Text>
-               <View style={{flexDirection: 'row', marginTop: 10}}>
-                <Ionic name='calendar-outline' size={16} color='#424242'></Ionic>
-                <Text style={styles.content}>{data.date} </Text>
-                <Text style={styles.content}>{data.day} </Text>
-                <Text style={styles.content}>{data.time}</Text>
-               </View>
-               <View style={{flexDirection: 'row', marginTop: 10}}>
-               <Ionic name='location-outline' size={16} color='#424242'></Ionic>
-                <Text style={styles.content}>{data.where}</Text>
-               </View>
-               <View style={{marginTop: 10}}>
-                <Text style={styles.content}>준비물: {data.meterials}</Text>
-                </View>
-                <View style={{marginTop: 40}}>
-                    <Text style={styles.content}>{data.content}</Text>
-                </View>
-                <View style={{marginBottom: 165}}></View>
-                {/* 참여하기버튼 */}
-                {!isParticipation ? (
-                <Pressable disabled={parComplete} style={[styles.button,{ backgroundColor: '#0BE060'}]} 
-                onPress={() => {
-                    setParComplete(true)
-                    Alert.alert('참여가 완료되었습니다.');
-                }}>
-                    <Text style={{color: 'white', fontSize: 20, fontWeight: 600, marginBottom: 10}}>참여하기</Text>
-                </Pressable>
-                ):(
-                    <Pressable disabled={parComplete} style={[styles.btn, { backgroundColor: '#CBCBCB' }]} 
-                    onPress={() => {
-                        setParComplete(true)
+                {/* 상단바 */}
+                <View 
+                    style={{
+                    width: '100%', 
+                    backgroundColor: 'white', 
+                    height: 44, 
+                    alignItems: 'center',
+                    paddingHorizontal: 18,
+                    flexDirection: 'row',
+                    justifyContent:'space-between',
                     }}>
-                        <Text style={{color: 'white', fontSize: 20, fontWeight: 600, marginBottom: 10}}>참여하기</Text> 
-                    </Pressable>
-                )}
 
-               
+                    {/* 뒤로가기  */}
+                    <TouchableOpacity>
+                        <Ionic name="chevron-back-sharp" style={{fontSize:24}} onPress={() => navigation.goBack()} />
+                    </TouchableOpacity>
+
+                    {/* 점 세개 */}
+                    <TouchableOpacity>
+                        <Ionic name= "md-ellipsis-horizontal" size={17} color='#424242' />
+                    </TouchableOpacity>
+
+                </View>
+
+                <View>
+                    <Image source={data.image} style={{width: '100%', height: 240}} />
+                </View>
+
+                <View style={styles.wrapper}>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <View style={styles.a}>
+                            <Text style={styles.situation}>{data.situation}</Text>
+                        </View>
+
+                        {/* 사람 아이콘 */}
+                        <Ionic style={styles.icon} name='person-outline' size={16} color="#424242"> 5</Ionic>
+                    
+                    </View>
+
+                    <Text style={styles.title}>
+                        {data.title}
+                    </Text>
+
+                    <View style={{flexDirection: 'row', marginTop: 20 ,marginVertical: 10}}>
+                        <Image source={data.avatar} style={styles.avatar}></Image>
+                        <Text style={styles.name}>{data.name}</Text>
+                    </View>
+
+                    <Text style={styles.subtitle}>모임정보</Text>
+
+                    {/* 요일정보 */}
+                    <View style={{flexDirection: 'row', marginTop: 10}}>
+                        {/* 달력 아이콘 */}
+                        <Ionic name='calendar-outline' size={16} color='#424242'></Ionic>
+                        <Text style={styles.content}>{data.date} </Text>
+                        <Text style={styles.content}>{data.day} </Text>
+                        <Text style={styles.content}>{data.time}</Text>
+                    </View>
+
+                    {/* 위치정보 */}
+                    <View style={{flexDirection: 'row', marginTop: 10}}>
+                        {/* 위치 아이콘 */}
+                        <Ionic name='location-outline' size={16} color='#424242'></Ionic>
+                        <Text style={styles.content}>{data.where}</Text>
+                    </View>
+
+                    {/* 준비물정보 */}
+                    <View style={{marginTop: 10}}>
+                        <Text style={styles.content}>준비물: {data.meterials}</Text>
+                    </View>
+
+                    <View style={{marginTop: 40}}>
+                        <Text style={styles.content}>{data.content}</Text>
+                    </View>
+                    
+                    <View style={{marginBottom: 165}}></View>
+                    
+        
+                </View>
             </View>
-        </View>
-    </SafeAreaView>
+        </SafeAreaView>
+        {!isParticipation ? (
+            <Pressable disabled={parComplete} style={[styles.button,{ backgroundColor: '#0BE060'}]} 
+                onPress={() => {
+                setParComplete(true)
+                Alert.alert('참여가 완료되었습니다.');
+            }}>
+                <Text style={{color: 'white', fontSize: 20, fontWeight: 600, marginBottom: 10}}>참여하기</Text>
+            </Pressable>
+            ):(
+            <Pressable disabled={parComplete} style={[styles.btn, { backgroundColor: '#CBCBCB' }]} 
+                onPress={() => {
+                setParComplete(true)
+            }}>
+                <Text style={{color: 'white', fontSize: 20, fontWeight: 600, marginBottom: 10}}>참여하기</Text> 
+            </Pressable>
+        )}
+    </View>
   )
 }
 
@@ -167,11 +186,4 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#CBCBCB' ,
     }
-
-    
-
-
-
-  
-
 });
